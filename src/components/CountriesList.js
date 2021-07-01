bcfd3c3c70c7d61dade96ba6efd5903d18d666a1
Tran Van <vanbaotran201194@gmail.com>
2021-07-01 21:25:09 +0200

    ite 1.1

---
+++ src/components/CountriesList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import json from '../countries.json'
+import {Link} from 'react-router-dom'
+
+function CountriesList(){
+    const list = [...json]
+    return (
+        <div>
+            {list.map((el,i)=>{
+                return (
+                    <div key={i}>
+                    <h4>
+                        <Link to={`/countries/${i}`}>{el.name.common}</Link>
+                    </h4>
+                    </div>
+                )
+            })}
+        </div>
+
+    )
+}
+export default CountriesList 
---
+++ src/components/NavBar.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function NavBar() {
+    return <nav className='navbar'>Wiki Countries</nav>
+}
+export default NavBar;
